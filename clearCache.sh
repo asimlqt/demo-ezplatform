5f03c7a635becce720abaf10b0a918a59772e97e
Kieren Evans <kevans@inviqa.com>
2017-05-09 10:04:59 +0100

    Cache clear via container command built into the symfony parent image

--- clearCache.sh
+++ clearCache.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-cp-remote exec -- php app/console cache:clear
-cp-remote exec -- chown -R build:www-data app/cache
-cp-remote exec -- chmod 775 -R app/cache
+cp-remote exec -- container cache_clear
